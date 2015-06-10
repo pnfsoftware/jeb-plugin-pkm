3a7084b8547936288cade2c79afeb66cfdb7104a
Carlos Gonzales <carlos@pnfsoftware.com>
2015-06-10 12:18:59 -0700

    Initial commit

---
+++ src/com/pnf/PkmPlugin.java
@@ -0,0 +1,42 @@
+package com.pnf;
+
+import com.pnfsoftware.jeb.core.PluginInformation;
+import com.pnfsoftware.jeb.core.properties.IPropertyDefinitionManager;
+import com.pnfsoftware.jeb.core.properties.IPropertyManager;
+import com.pnfsoftware.jeb.core.units.AbstractUnitIdentifier;
+import com.pnfsoftware.jeb.core.units.IBinaryFrames;
+import com.pnfsoftware.jeb.core.units.IUnit;
+import com.pnfsoftware.jeb.core.units.IUnitProcessor;
+
+public class PkmPlugin extends AbstractUnitIdentifier{
+	private static int[] PKM_MAGIC = {(byte) 0x50, (byte) 0x4B, (byte) 0x4D, (byte) 0x20, (byte) 0x31, (byte) 0x30};
+	private static String ID = "pkm_plugin";
+	
+	public PkmPlugin() {
+		super(ID, 0);
+	}
+
+	public boolean identify(byte[] stream, IUnit unit) {
+		return checkBytes(stream, 0, PKM_MAGIC);
+	}
+	
+	public void initialize(IPropertyDefinitionManager parent, IPropertyManager pm) {
+        super.initialize(parent, pm);
+        /** Add any necessary property definitions here **/
+    }
+
+	@Override
+	public IUnit prepare(String name, byte[] data, IUnitProcessor processor, IUnit unit) {
+		return null;
+	}
+
+	@Override
+	public PluginInformation getPluginInformation() {
+		return new PluginInformation("PKM Plugin", "", "1.0", "PNF Software");
+	}
+
+	@Override
+	public IUnit reload(IBinaryFrames data, IUnitProcessor processor, IUnit unit) {
+		return null;
+	}
+}
