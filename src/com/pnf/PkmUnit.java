3b37e13aefea6a3bc764309b6b7ae11e981e1ab7
Carlos Gonzales <carlos@pnfsoftware.com>
2015-06-11 11:55:48 -0700

    Make use of new status api

--- src/com/pnf/PkmUnit.java
+++ src/com/pnf/PkmUnit.java
@@ -20,22 +20,6 @@ public class PkmUnit extends AbstractBinaryUnit {
 
 	public PkmUnit(String name, byte[] data, IUnitProcessor unitProcessor, IUnit parent, IPropertyDefinitionManager pdm) {
 		super(null, data, TYPE, name, unitProcessor, parent, pdm);
-
-		// Make sure we can find the tool 
-		/** TODO Don't hardcode this path **/
-		File platformTools = new File("C:\\Users\\carlos\\Downloads\\jeb-2.0.0.201505271805-CarlosGonzales-bBA-631586206-315243500646364846\\bin");//getPropertyManager().getString(PkmPlugin.PROP_NAME));
-		File etcTool = null;
-		File[] files = platformTools.listFiles();
-
-		for(File f: files){
-			if(f.getName().contains(TOOL))
-				etcTool = f;
-		}
-
-		if(etcTool == null || !etcTool.exists())
-			throw new RuntimeException("Could not find etc1tool in directory " + platformTools.getAbsolutePath());
-
-		pkmTool = new PkmTool(name, etcTool, data);
 	}
 
 	public String getDescription(){
@@ -43,6 +27,36 @@ public class PkmUnit extends AbstractBinaryUnit {
 	}
 
 	public boolean process(){
+		// Retrieve property entered by user
+		File platformTools = new File(getPropertyManager().getString(PkmPlugin.PROP_NAME));
+
+		// Terminate early if platformTools file is not valid
+		if(!platformTools.exists() || !platformTools.isDirectory()){
+			processed = false;
+			setStatus(platformTools.getAbsolutePath() + " is not a directory.");
+
+			return processed;
+		}
+
+		File etcTool = null;
+		File[] files = platformTools.listFiles();
+
+		// Look for etc1tool anywhere in the directory specified by the given path
+		for(File f: files){
+			if(f.getName().contains(TOOL))
+				etcTool = f;
+		}
+
+		// Make sure we can find the tool
+		if(etcTool == null || !etcTool.exists()){
+			processed = false;
+			setStatus("Could not find etc1tool in directory " + platformTools.getAbsolutePath());
+
+			return processed;
+		}
+
+		pkmTool = new PkmTool(name, etcTool, data);
+
 		File png = pkmTool.dumpPng();
 		IUnit sub = null;
 
